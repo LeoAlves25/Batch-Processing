2c9a4bf91e5c33d73b1259e3cc318efc370923d0
Leonardo Castro Alves <leo_alves25@yahoo.com.br>
2023-09-13 13:42:54 -0300

    Enviando massa de dados para BD

--- batch-processing-demo/src/main/java/com/javatechie/batchprocessingdemo/config/CustomerProcessor.java
+++ batch-processing-demo/src/main/java/com/javatechie/batchprocessingdemo/config/CustomerProcessor.java
@@ -7,9 +7,8 @@ import com.javatechie.batchprocessingdemo.entities.Customer;
 public class CustomerProcessor implements ItemProcessor<Customer, Customer>{
 
     @Override
-    public Customer process(Customer item) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    public Customer process(Customer customer) throws Exception {
+        return customer;
     }
     
 }
--- batch-processing-demo/src/main/java/com/javatechie/batchprocessingdemo/config/SpringBatchConfig.java
+++ batch-processing-demo/src/main/java/com/javatechie/batchprocessingdemo/config/SpringBatchConfig.java
@@ -1,8 +1,9 @@
 package com.javatechie.batchprocessingdemo.config;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -13,6 +14,8 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.javatechie.batchprocessingdemo.entities.Customer;
@@ -21,17 +24,10 @@ import com.javatechie.batchprocessingdemo.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 
 @Configuration
-@EnableBatchProcessing
 @AllArgsConstructor
 public class SpringBatchConfig {
-    
-    private JobBuilder jobBuilder;
-
-    private StepBuilder stepBuilder;
 
     private CustomerRepository customerRepository;
-
-    private PlatformTransactionManager transactionManager;
 
 
     @Bean
@@ -75,12 +71,29 @@ public class SpringBatchConfig {
         return itemWriter;
     }
 
-    public Step step1(){
-        return stepBuilder
-                .<Customer, Customer>chunk(100, transactionManager)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                .build();
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager){
+        return new StepBuilder("step1", jobRepository)
+                    .<Customer, Customer>chunk(100, transactionManager)
+                    .reader(reader())
+                    .processor(processor())
+                    .writer(writer())
+                    .taskExecutor(taskExecutor())
+                    .build();
+    }
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step1){
+        return new JobBuilder("job", jobRepository)
+                    .start(step1)
+                    .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor(){
+        SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
+        taskExecutor.setConcurrencyLimit(10);
+
+        return taskExecutor;
     }
 }
